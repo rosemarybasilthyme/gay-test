fa5b4db0e813bcee340b6aca6b820e3a7ee03dcb
rosemarybasilthyme <144396914+rosemarybasilthyme@users.noreply.github.com>
2023-09-21 15:32:18 -0500

    Add files via upload

---
+++ gaytest.py
@@ -0,0 +1,6 @@
+import webbrowser
+import os
+print("are you gay?")
+gay=input("y/n   ")
+if gay=="y" or gay=="yes": print("nice"), webbrowser.open('https://www.tumblr.com/')
+if gay=="n" or gay=="no": print("cringe"), webbrowser.open('https://trollface.dk/')
